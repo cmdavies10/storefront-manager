bde81686318dd7469873c3fda698d7f65c01a0c5
Chris Davies <c.m.davies10@gmail.com>
2019-10-28 12:37:49 -0400

    schema and seeds sql files updated

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("computer", "electronics", 1500.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("chair", "furniture", 100.25, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("camera", "electronics", 2500.00, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("desk", "furtniture", 499.01, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("fan", "furniture", 10.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("apple", "food", 1.99, 36);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("banana", "food", 3.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("computer monitor", "electronics", 150.25, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("computer keyboard", "electronics", 99.99, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("computer cord", "electronics", 0.99, 3);
